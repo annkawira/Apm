c6569d3e32ea0cb6eb354949c30a71ba526530ea
Ann K. Mwobi <ann.mwobi@calltronix.com>
2022-02-15 11:43:22 +0300

    files added

--- Apm/src/app/app.module.ts
+++ Apm/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { productListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    productListComponent
   ],
   imports: [
     BrowserModule,
