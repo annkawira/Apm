61ccb1d39edebef3780a3be05e6396d77d3c47f7
Ann K. Mwobi <ann.mwobi@calltronix.com>
2022-02-16 09:37:42 +0300

    product details added

--- Apm/src/app/app.module.ts
+++ Apm/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { productListComponent } from './products/product-list.component';
+import { ProductDetailsComponent } from './products/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    productListComponent
+    productListComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
